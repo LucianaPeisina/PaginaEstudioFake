d335f6ee731771d2a485c0b4235af32f6c53546a
LucianaPeisina <lucianapeisina@hotmail.com>
2022-10-17 12:26:37 -0300

    Agrego el JS que tiene todo el header, lo uno en cada pagina, y voy viendo como se ve la pagina de servicios

--- navegacion.js
+++ navegacion.js
@@ -1,26 +1,30 @@
-/* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
-  const boton = document.querySelector('.boton')
-  const navC    = document.querySelector('.navC')
-  
-  boton.addEventListener('click',()=>{
-      navC.classList.toggle('activo')
-  })
+//ACCIONES PARA LA PAGINA
 
-  /*sliders en INDEX*/
-  
+//Menu hamburguesa
+
+/* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
+const boton = document.querySelector('.boton')
+const navC    = document.querySelector('.navC')
+
+boton.addEventListener('click',()=>{
+    navC.classList.toggle('activo')
+})
+
+/*sliders en INDEX*/
+
 let slides = document.querySelectorAll('.contenedorI');
 let index = 0;
 
 function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
 }
 
 function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
 }
 
 setInterval(next, 7000);
@@ -30,13 +34,13 @@ var acc = document.getElementsByClassName("acordeon");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var contenido = this.nextElementSibling;
-    if (contenido.style.maxHeight) {
-      contenido.style.maxHeight = null;
-    } else {
-      contenido.style.maxHeight = contenido.scrollHeight + "px";
-    } 
-  });
+acc[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var contenido = this.nextElementSibling;
+  if (contenido.style.maxHeight) {
+    contenido.style.maxHeight = null;
+  } else {
+    contenido.style.maxHeight = contenido.scrollHeight + "px";
+  } 
+});
 }
