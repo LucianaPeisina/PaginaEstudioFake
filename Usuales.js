3e3f485dc64adc7413663eeaa1de9b5b9b00b27b
LucianaPeisina <lucianapeisina@hotmail.com>
2022-10-18 21:45:24 -0300

    Agregar el footer al JS Usuales, modificacion del Json, Agreado estilos a EstilosClientes, modificado nombre de Nuestros clientes a Noticias, futuros cambios de esa pagina a responsibe y cambio de nombre a esa hoja

--- Usuales.js
+++ Usuales.js
@@ -11,7 +11,7 @@ let Bnav=`
         <li class="navC-item"><a class="link" href="index.html"> Inicio </a></li>
         <li class="navC-item"><a class="link" href="Servicios.html"> Servicios</a></li>
         <li class="navC-item"><a class="link" href="Sobre Nosotros.html">Sobre Nosotros</a> </li>
-        <li class="navC-item"> <a class= "link" href="Nuestros Clientes.html">Clientes</a></li>
+        <li class="navC-item"> <a class= "link" href="Nuestros Clientes.html">Noticias</a></li>
         <li class="navC-item"><a class= "link" href="Contacto.html">Contactanos</a></li>
     </ul>
 </nav> 
@@ -19,126 +19,7 @@ let Bnav=`
 
 document.querySelector(".headerC").innerHTML = Bnav;
 
-// PAGINA INDEX
-let PIndex= `
-<div class="Sliders">
-<div class="contenedorI active">
-    <div class="slide"> 
-        <div class="slideproject"> 
-            <p> Le ayudaremos a tramitar lo necesario para su jubilación.</p>
-            <br>
-            <a href="./Contacto.html" class="BotonConsulta"> Haga su consulta aquí</a>
-        </div>
-    </div>  
-</div>
-<div class="contenedorI">
-    <div class="slide"> 
-        <div class="slideproject"> 
-            <p> Tiene derecho a un cálculo de reajuste de haberes justo.</p>
-            <br>
-            <a href="./Contacto.html" class="BotonConsulta"> Hacé tu consulta hoy</a>
-        </div>
-    </div>  
-</div>
-<div class="contenedorI">
-    <div class="slide"> 
-        <div class="slideproject"> 
-            <p> Confie en nosotros y le brindaremos un servicio de calidad. </p>
-            <br>
-            <a href="./Contacto.html" class="BotonConsulta"> Hacé tu consulta aquí</a>
-        </div>
-    </div>  
-</div>
-</div>
-<div class="acordeones">
-<h2 class="Servicios">Servicios </h2>
-<button class="acordeon">Asesoramiento Previsional</button>
-<div class="contenido">
-    <ul class="listasS">
-            <li class="ServiciosS">Gestión de jubilación</li>
-            <li class="ServiciosS"> Consulta profesional</li>
-            <li class="ServiciosS"> Informe Previsional con Cálculos</li>
-            <li class="ServiciosS"> Reconocimiento de Haberes</li>
-            <li class="ServiciosS">Retiro por Invalidez</li>
-            <li class="ServiciosS">Pensión por fallecimiento</li>
-    </ul>
-    <br>
-    <div class="botonesS"> 
-        <a href="./Servicios.html" class="BotonServicios"> Quiero saber más</a>
-        <a href="./Contacto.html" class="BotonConsultor"> Enviar Consulta</a>
-    </div>
-</div>
-<button class="acordeon">Reajuste de Haberes</button>
-<div class="contenido">
-    <ul class="listasS">
-        <li class="ServiciosS">Gestión de jubilación</li>
-        <li class="ServiciosS"> Consulta profesional</li>
-        <li class="ServiciosS"> Informe Previsional con Cálculos</li>
-        <li class="ServiciosS"> Reconocimiento de Haberes</li>
-        <li class="ServiciosS">Retiro por Invalidez</li>
-        <li class="ServiciosS">Pensión por fallecimiento</li>
-    </ul>
-    <br>
-    <div class="botonesS"> 
-        <a href="./Servicios.html" class="BotonServicios"> Quiero saber más</a>
-        <a href="./Contacto.html" class="BotonConsultor"> Enviar Consulta</a>
-    </div>
-</div>
-<button class="acordeon">Sucesiones</button>
-<div class="contenido">
-    <ul class="listasS">
-        <li class="ServiciosS">Gestión de jubilación</li>
-        <li class="ServiciosS"> Consulta profesional</li>
-        <li class="ServiciosS"> Informe Previsional con Cálculos</li>
-        <li class="ServiciosS"> Reconocimiento de Haberes</li>
-        <li class="ServiciosS">Retiro por Invalidez</li>
-        <li class="ServiciosS">Pensión por fallecimiento</li>
-    </ul>
-    <br>
-    <div class="botonesS"> 
-        <a href="./Servicios.html" class="BotonServicios"> Quiero saber más</a>
-        <a href="./Contacto.html" class="BotonConsultor"> Enviar Consulta</a>
-    </div>
-</div>
-<button class="acordeon">Jubilación</button>
-<div class="contenido">
-    <ul class="listasS">
-        <li class="ServiciosS">Gestión de jubilación</li>
-        <li class="ServiciosS"> Consulta profesional</li>
-        <li class="ServiciosS"> Informe Previsional con Cálculos</li>
-        <li class="ServiciosS"> Reconocimiento de Haberes</li>
-        <li class="ServiciosS">Retiro por Invalidez</li>
-        <li class="ServiciosS">Pensión por fallecimiento</li>
-    </ul>
-    <br>
-    <div class="botonesS"> 
-        <a href="./Servicios.html" class="BotonServicios"> Saber más</a>
-        <a href="./Contacto.html" class="BotonConsultor"> Enviar Consulta</a>
-    </div>
-</div>
- `
-
- document.getElementById("Index").innerHTML = PIndex;
-//FIN PAGINA INDEX
-
-//PAGINA SERVICIOS
-
-
-
-//FIN PAGINA SERVICIOS
-
-//PAGINA SOBRE NOSOTROS
-
-//FIN PAGINA SOBRE NOSOTROS
-
-// PAGINA NUESTROS CLIENTES
-
-// FIN PAGINA SOBRE NOSOTROS
-
-// PAGINA CONTACTO
-
-// FIN PAGINA CONTACTO
-// Footer  
+//Footer
 let Pfooter=`    
 <div class="red"> 
 <img src="./imagenes/fake.jpg" alt="logo" class="logo-footer"> 
@@ -171,5 +52,5 @@ let Pfooter=`
 </div>--> 
 ` ;
 
-document.querySelector(".FooterP").innerHTML= Pfooter;
-
+document.getElementById("FooterP").innerHTML= Pfooter;
+//Probabando el JSON de clientes
